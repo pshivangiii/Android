9242f37a49dabaad2d7f51fb1344ce63e4dd8386
pshivangiii <99738066+pshivangiii@users.noreply.github.com>
2022-06-09 17:12:59 +0530

    Update ContactApiWithoutFragment.kt

--- Register/app/src/main/java/com/example/register/ContactApiWithoutFragment.kt
+++ Register/app/src/main/java/com/example/register/ContactApiWithoutFragment.kt
@@ -1,6 +1,5 @@
 package com.example.register
 
-import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -17,7 +16,7 @@ import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 
 
-class ContactApiActivity : AppCompatActivity()
+class ContactApiWithoutFragment : AppCompatActivity()
 {
     val data = ArrayList<ItemsViewModel>()
     private lateinit var sharedPreferences: SharedPreferences
@@ -29,15 +28,8 @@ class ContactApiActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_api)
-
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         val token = sharedPreferences.getString(getString(R.string.name), "")
-        if (token.isNullOrEmpty())
-        {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            return
-        }
         recyclerView=findViewById(R.id.recyclerview)
         getData(token,pageNumber)
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener()
@@ -58,51 +50,49 @@ class ContactApiActivity : AppCompatActivity()
     }
 
     private fun getData(token: String?, page: Int) {
+
         //CALLING CONTACT API
         val queue = Volley.newRequestQueue(this)
         val progressBar:ProgressBar= findViewById(R.id.progressBar)
         val url = "https://api-smartflo.tatateleservices.com/v1/contacts?page=$page"
+
         val stringRequest = object : StringRequest(
             Request.Method.GET, url,
             Response.Listener<String>
             { response ->
                 val json = JSONObject(response)
-                var size = json.getString("size")
+                val size = json.getString("size")
                 val jsonArray = json.getJSONArray("results")
                 if (size.toInt() == 0)
                 {
                     progressBar.visibility = View.INVISIBLE
                     Toast.makeText(this, "No more data", Toast.LENGTH_SHORT).show()
-
                 }
-                else
+                recyclerView.layoutManager = LinearLayoutManager(this)
+                for (i in 0 until jsonArray.length())
                 {
-                    recyclerView.layoutManager = LinearLayoutManager(this)
-                    for (i in 0 until jsonArray.length())
-                    {
-                        progressBar.visibility = View.INVISIBLE
-                        val jsonObj = jsonArray.getJSONObject(i)
-                        val name = jsonObj.getString("name")
-                        val phoneNumber = jsonObj.getString("phone_number")
-                        data.add(ItemsViewModel("NAME : " + name, "phoneNumber : " + phoneNumber))
-                    }
-                    progressBar.visibility = View.VISIBLE
-                    val adapter = CustomAdapter(data)
-                    // Setting the Adapter with the recyclerview
-                    recyclerView.adapter = adapter
-                    adapter.setOnLoadMoreListener()
-                    adapter.notifyDataSetChanged();
+                    progressBar.visibility = View.INVISIBLE
+                    val jsonObj = jsonArray.getJSONObject(i)
+                    val name = jsonObj.getString("name")
+                    val phoneNumber = jsonObj.getString("phone_number")
+                    data.add(ItemsViewModel("NAME : " + name, "phoneNumber : " + phoneNumber))
                 }
+                progressBar.visibility = View.VISIBLE
+                val adapter = CustomAdapter(data)
+                // Setting the Adapter with the recyclerview
+                recyclerView.adapter = adapter
+                adapter.setOnLoadMoreListener()
+                adapter.notifyDataSetChanged()
             },
             Response.ErrorListener
             {
                 Toast.makeText(this, "INVALID", Toast.LENGTH_SHORT).show()
             })
-            {
+        {
             //adding header
             override fun getHeaders(): Map<String, String> {
                 val headers = HashMap<String, String>()
-                headers["Content-Type"] = "application/json";
+                headers["Content-Type"] = "application/json"
                 headers["Authorization"] = "Bearer " + token.toString()
                 return headers
             }
